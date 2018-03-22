34726449773d14bec284a5ec568e698b33595e12
William Henry <overtaker44@hotmail.com>
2018-03-22 13:56:56 -0400

    darts working

---
+++ ATS_Demo/Assets/DartKill.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DartKill : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.collider.CompareTag("TileMap"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    }
