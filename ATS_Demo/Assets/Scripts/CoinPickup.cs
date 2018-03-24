13ea2d198065307557be8c1c6d59426a0c6ad01f
Carter Boon <booncarter@gamil.com>
2018-03-24 14:44:05 -0400

    Missed a file in previous push - added here

--- ATS_Demo/Assets/Scripts/CoinPickup.cs
+++ ATS_Demo/Assets/Scripts/CoinPickup.cs
@@ -8,8 +8,14 @@ public class CoinPickup : MonoBehaviour {
     public int coins;
     public Text coinsText;
 
+    private void Start()
+    {
+        coins = GameControl.control.getCoins();
+    }
+
     void Update()
     {
+        coins = GameControl.control.getCoins();
         coinsText.text = ("Coins: " + coins);
     }
 }
