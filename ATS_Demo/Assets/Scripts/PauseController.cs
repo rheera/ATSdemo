31d4fc0b2c769c00df1a74768efad310c741061f
Carter Boon <booncarter@gamil.com>
2018-03-26 09:20:03 -0400

    Added the ability to muffle sound on pause

--- ATS_Demo/Assets/Scripts/PauseController.cs
+++ ATS_Demo/Assets/Scripts/PauseController.cs
@@ -1,21 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class PauseController : MonoBehaviour {
 
     public static bool GamePaused = false;
     public GameObject pauseMenu;
     private bool tutorialText = false;
+    public AudioMixer musicMasterMixer;
+    public AudioMixer sfxMasterMixer;
+    private float normalMusicVolume, normalSFXVolume;
 
-	// Update is called once per frame
-	void Update () {
+    private void Awake()
+    {
+        normalMusicVolume = GetMasterLevel(musicMasterMixer);
+        normalSFXVolume = GetMasterLevel(sfxMasterMixer);
+    }
+
+    // Update is called once per frame
+    void Update () {
         if (Input.GetKeyDown(KeyCode.Escape)) {
             if (GamePaused)
             {
+                musicMasterMixer.SetFloat("musicMasterVolume", normalMusicVolume);
+                sfxMasterMixer.SetFloat("soundEffectsMasterVolume", normalSFXVolume);
+
                 Resume();
             }
             else {
+                sfxMasterMixer.SetFloat("soundEffectsMasterVolume", -20f);
+                musicMasterMixer.SetFloat("musicMasterVolume", -20f);
                 Pause();
             }
         }
@@ -52,6 +67,20 @@ public class PauseController : MonoBehaviour {
     public void Load() {
         SceneControl.control.Load();
         GameControl.control.Load();
+    }
+
+    float GetMasterLevel(AudioMixer masterMixer)
+    {
+        float value;
+        bool result = masterMixer.GetFloat("masterVolume", out value);
+        if (result)
+        {
+            return value;
+        }
+        else
+        {
+            return 0f;
+        }
     }
 
 }
